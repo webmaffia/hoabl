d64fafaf80bb02586e4d19bab9fea085042dbff7
Javed <javed@marketinsider.in>
2025-08-10 18:35:19 +0530

    first commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  distDir: 'build',
   output: 'standalone',
-   eslint: {
+  eslint: {
     ignoreDuringBuilds: true,
   },
 };
